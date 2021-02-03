a29f825db888a9517c26cce864d83cf28db5062c
Andres Osante <aosantea@ucenfotec.ac.cr>
2021-02-02 22:34:35 -0600

    some comment

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -30,8 +30,7 @@ function Counter({initialCount = 0, step = 1}) {
   const [state, dispatch] = useReducer(countReducer, {count: initialCount});
   const {count} = state
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
+  // 💰 you can write the countReducer function so you don't have to make any changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
   const increment = () => dispatch({type: 'INCREMENT', step})
