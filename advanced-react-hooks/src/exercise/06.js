d2ce93ed2436971f4e3b866ce2d25a154379fe50
Andres Osante <aosantea@ucenfotec.ac.cr>
2021-02-16 22:42:47 -0600

    useDebugValue unit test

--- advanced-react-hooks/src/exercise/06.js
+++ advanced-react-hooks/src/exercise/06.js
@@ -6,13 +6,14 @@
 
 import * as React from 'react'
 
-const formatDebugValue = ({query, state}) => `\`${query}\` => ${state}`
+// const formatDebugValue = ({query, state}) => `\`${query}\` => ${state}`
 
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
-  React.useDebugValue({query, state}, formatDebugValue)
+  React.useDebugValue(`\`${query}\` => ${state}`)
+  // React.useDebugValue({query, state}, formatDebugValue)
 
   React.useEffect(() => {
     let mounted = true
