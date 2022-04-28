2645c1724c1d366a76c5fff319512e78a8a02d76
aosante <aosantea@ucenfotec.ac.cr>
2022-04-28 12:09:19 -0600

    complete useMemo workshop

--- react-performance/src/exercise/02.js
+++ react-performance/src/exercise/02.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
 import {getItems} from '../filter-cities'
+// import {getItems} from '../workerized-filter-cities'
 import {useForceRerender} from '../utils'
 
 function Menu({
@@ -60,8 +61,15 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
+  // Web Workers example
+
+  // const {data: allItems, run} = useAsync({data: [], status: 'pending'})
+  // React.useEffect(() => {
+  //   run(getItems(inputValue))
+  // }, [inputValue, run])
+
   // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
   const items = allItems.slice(0, 100)
 
   const {
