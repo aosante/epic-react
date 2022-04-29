9eb1c840dfbbd26d22956a99792a996741d63274
aosante <aosantea@ucenfotec.ac.cr>
2022-04-28 21:29:14 -0600

    add React.Profiler component for production montiroing of app performance

--- react-performance/src/__tests__/07.js
+++ react-performance/src/__tests__/07.js
@@ -2,8 +2,8 @@ import * as React from 'react'
 import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
 import {render} from '@testing-library/react'
 import reportProfile from '../report-profile'
-import App from '../final/07'
-// import App from '../exercise/07'
+// import App from '../final/07'
+import App from '../exercise/07'
 
 jest.mock('react', () => {
   const actualReact = jest.requireActual('react')
