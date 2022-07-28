083be317bab8bb6d89a39383287af8e7e0885cee
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-07-27 20:52:25 -0600

    testing custom hook part 1

--- testing-react-apps/src/__tests__/exercise/08.js
+++ testing-react-apps/src/__tests__/exercise/08.js
@@ -9,14 +9,33 @@ import useCounter from '../../components/use-counter'
 // 🐨 create a simple function component that uses the useCounter hook
 // and then exposes some UI that our test can interact with to test the
 // capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+const UseCounterHookComponent = () => {
+  const {count, increment, decrement} = useCounter()
+  return (
+    <div>
+      <div>
+        <div>Current count: {count}</div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  )
+}
 
-test('exposes the count and increment/decrement functions', () => {
+test('exposes the count and increment/decrement functions', async () => {
   // 🐨 render the component
+  render(<UseCounterHookComponent />)
   // 🐨 get the elements you need using screen
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const message = screen.getByText(/current count/i)
   // 🐨 assert on the initial state of the hook
+  expect(message).toHaveTextContent('Current count: 0')
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  await userEvent.click(increment) // userEvent fires all the events that really happen when the user interacts with the button (not just the click event)
+  expect(message).toHaveTextContent('Current count: 1')
+  await userEvent.click(decrement)
+  expect(message).toHaveTextContent('Current count: 0')
 })
 
 /* eslint no-unused-vars:0 */
