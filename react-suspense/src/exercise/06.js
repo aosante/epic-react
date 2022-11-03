2665efaa502fabee795bea3d53fea64a421b874b
aosante <aosantea@ucenfotec.ac.cr>
2022-11-02 22:13:25 -0600

    suspense with custom hook

--- react-suspense/src/exercise/06.js
+++ react-suspense/src/exercise/06.js
@@ -48,15 +48,10 @@ function createPokemonResource(pokemonName) {
   return {data, image}
 }
 
-function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
-  // 🐨 move these two lines to a custom hook called usePokemonResource
+const usePokemonResource = (pokemonName) => {
   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
   const [pokemonResource, setPokemonResource] = React.useState(null)
-  // 🐨 call usePokemonResource with the pokemonName.
-  //    It should return both the pokemonResource and isPending
 
-  // 🐨 move this useEffect call to your custom usePokemonResource hook
   React.useEffect(() => {
     if (!pokemonName) {
       setPokemonResource(null)
@@ -66,6 +61,20 @@ function App() {
       setPokemonResource(getPokemonResource(pokemonName))
     })
   }, [pokemonName, startTransition])
+
+  return [pokemonResource, isPending]
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+  // 🐨 move these two lines to a custom hook called usePokemonResource
+  // const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
+  // const [pokemonResource, setPokemonResource] = React.useState(null)
+  // 🐨 call usePokemonResource with the pokemonName.
+  //    It should return both the pokemonResource and isPending
+  const [pokemonResource, isPending] = usePokemonResource(pokemonName)
+
+  // 🐨 move this useEffect call to your custom usePokemonResource hook
 
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
