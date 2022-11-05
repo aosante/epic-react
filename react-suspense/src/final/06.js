405e2d092fcdc29793b952cbebafb4a2b87fa03d
Andres Osante <aosantea@ucenfotec.ac.cr>
2022-11-05 13:29:27 -0600

    usePokemonResource custom hook

--- react-suspense/src/final/06.js
+++ react-suspense/src/final/06.js
@@ -3,14 +3,15 @@
 
 import * as React from 'react'
 import {
-  fetchPokemon,
-  getImageUrlForPokemon,
+  // fetchPokemon,
+  // getImageUrlForPokemon,
   PokemonInfoFallback,
   PokemonForm,
   PokemonDataView,
   PokemonErrorBoundary,
+  usePokemonResource,
 } from '../pokemon'
-import {createResource, preloadImage} from '../utils'
+// import {createResource, preloadImage} from '../utils'
 
 function PokemonInfo({pokemonResource}) {
   const pokemon = pokemonResource.data.read()
@@ -24,46 +25,46 @@ function PokemonInfo({pokemonResource}) {
   )
 }
 
-const SUSPENSE_CONFIG = {
-  timeoutMs: 4000,
-  busyDelayMs: 300,
-  busyMinDurationMs: 700,
-}
+// const SUSPENSE_CONFIG = {
+//   timeoutMs: 4000,
+//   busyDelayMs: 300,
+//   busyMinDurationMs: 700,
+// }
 
-const pokemonResourceCache = {}
+// const pokemonResourceCache = {}
 
-function getPokemonResource(name) {
-  const lowerName = name.toLowerCase()
-  let resource = pokemonResourceCache[lowerName]
-  if (!resource) {
-    resource = createPokemonResource(lowerName)
-    pokemonResourceCache[lowerName] = resource
-  }
-  return resource
-}
+// function getPokemonResource(name) {
+//   const lowerName = name.toLowerCase()
+//   let resource = pokemonResourceCache[lowerName]
+//   if (!resource) {
+//     resource = createPokemonResource(lowerName)
+//     pokemonResourceCache[lowerName] = resource
+//   }
+//   return resource
+// }
 
-function createPokemonResource(pokemonName) {
-  const data = createResource(fetchPokemon(pokemonName))
-  const image = createResource(preloadImage(getImageUrlForPokemon(pokemonName)))
-  return {data, image}
-}
+// function createPokemonResource(pokemonName) {
+//   const data = createResource(fetchPokemon(pokemonName))
+//   const image = createResource(preloadImage(getImageUrlForPokemon(pokemonName)))
+//   return {data, image}
+// }
 
-function usePokemonResource(pokemonName) {
-  const [pokemonResource, setPokemonResource] = React.useState(null)
-  const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
+// function usePokemonResource(pokemonName) {
+//   const [pokemonResource, setPokemonResource] = React.useState(null)
+//   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
 
-  React.useEffect(() => {
-    if (!pokemonName) {
-      setPokemonResource(null)
-      return
-    }
-    startTransition(() => {
-      setPokemonResource(getPokemonResource(pokemonName))
-    })
-  }, [pokemonName, startTransition])
+//   React.useEffect(() => {
+//     if (!pokemonName) {
+//       setPokemonResource(null)
+//       return
+//     }
+//     startTransition(() => {
+//       setPokemonResource(getPokemonResource(pokemonName))
+//     })
+//   }, [pokemonName, startTransition])
 
-  return [pokemonResource, isPending]
-}
+//   return [pokemonResource, isPending]
+// }
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
